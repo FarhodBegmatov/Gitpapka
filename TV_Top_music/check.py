0bf2407d9c67afc2b7102912ffdac33563e600a2
Abduvohid <abduvohid.mirzamahmudov@gmail.com>
2021-01-13 21:44:23 +0500

    Tvtopmusic python project

---
+++ TV_Top_music/check.py
@@ -0,0 +1,7 @@
+from oop import *
+def userName():
+    name=open("name.txt","r")
+    name.read()
+
+
+
---
+++ TV_Top_music/main.py
@@ -0,0 +1,6 @@
+from oop import *
+print(bcolors.OKBLUE+"🎧"+bcolors.ENDC+bcolors.WARNING+"       ***Welcome to TV Top Music***"+bcolors.ENDC)
+menu()
+item = (input('Select one option:'))
+headMenu(item)
+
